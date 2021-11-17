3a412c65da138cc443fd5654e1340e29acc7538a
JieSun1990 <jie.sun1990@gmail.com>
2021-11-17 09:52:25 -0500

    added DualNum.py test_DualNum.py

---
+++ src/DualNum.py
@@ -0,0 +1,120 @@
+import numpy as np
+
+class DualNum:
+    def __init__(self, val, der, seed = np.array([1])):
+        self.val = val
+        self.der = der
+
+    # Overload addition
+    def __add__(self, other): 
+        try:
+            return DualNum(self.val + other.val, self.der + other.der) 
+        except:
+            other = DualNum(other, 0)
+            return DualNum(self.val + other.val, self.der + other.der)
+
+    # Overload radd
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    # Overload multiplication
+    def __mul__(self, other):
+        try:
+            return DualNum(self.val * other.val, self.val * other.der + other.val * self.der)
+        except:
+            other = DualNum(other, 0)
+            return DualNum(self.val * other.val, self.val * other.der + other.val * self.der)
+
+    # Overload rmul
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    # Overload subtraction
+    def __sub__(self, other):
+        return self.__add__(-other)
+
+    # Overload rsub
+    def __rsub__(self, other):
+        return (-self).__add__(other)
+
+    # Overload division
+    def __truediv__(self, other):
+        try:
+            return DualNum(self.val / other.val, (self.der * other.val - self.val * other.der)/(other.val**2))
+        except:
+            other=DualNum(other,0)
+            return DualNum(self.val / other.val, (self.der * other.val - self.val * other.der)/(other.val**2))
+
+    # Overload rdiv
+    # def __rtruediv__(self, other):
+    #     try:
+    #         return DualNum(other.val / self.val, (other.der * self.val - other.val * self.der)/(self.val**2))
+    #     except:
+    #         other=DualNum(other,0)
+    #         return DualNum(other.val / self.val, (other.der * self.val - other.val * self.der)/(self.val**2))
+
+    # Overload negation
+    def __neg__(self):
+        return DualNum(-self.val, -self.der)
+
+    # Overload power
+    def __pow__(self, exponent):
+        try:
+            return DualNum(self.val ** exponent.val, np.exp(exponent.val * np.log(self.val)) * (exponent.der * np.log(self.val) + (exponent.val / self.val) * self.der))
+        except:
+            exponent=DualNum(exponent,0)
+            return DualNum(self.val ** exponent.val, np.exp(exponent.val * np.log(self.val)) * (exponent.der * np.log(self.val) + (exponent.val / self.val) * self.der))
+
+
+    # #I don't think this is necessary?
+    # Overload rpow
+    # def __rpow__(self, exponent):
+    #     return exponent.__pow__(self)
+
+    # Overload sin
+    @staticmethod  
+    def sin(other):
+        try:
+            return DualNum(np.sin(other.val), np.cos(other.val)*other.der)
+        except:
+            other=DualNum(other,0)
+            return DualNum(np.sin(other.val), np.cos(other.val)*other.der)
+
+    # Overload cos
+    @staticmethod
+    def cos(other):
+        try:
+            return DualNum(np.cos(other.val), -np.sin(other.val)*other.der)
+        except:
+            other=DualNum(other,0)
+            return DualNum(np.cos(other.val), -np.sin(other.val)*other.der)
+
+    # Overload exp
+    @staticmethod
+    def exp(other):
+        try:
+            return DualNum(np.exp(other.val), np.exp(other.val)*other.der)
+        except:
+            other=DualNum(other,0)
+            return DualNum(np.exp(other.val), np.exp(other.val)*other.der)
+
+    # Overload ln
+    @staticmethod
+    def ln(other):
+        try:
+            return DualNum(np.log(other.val), 1/other.val*other.der)
+        except:
+            other=DualNum(other,0)
+            return DualNum(np.log(other.val), 1/other.val*other.der)
+
+"""
+#Some simple tests
+y=2*DualNum.sin(DualNum(0,1))+3
+print(y.val,y.der)
+y=DualNum.sin(5)
+print(y.der,y.val)
+y=10*DualNum(1,1)**3+3*DualNum(1,1)**5
+print(y.val,y.der)
+"""
+
+
---
+++ tests/test_DualNum.py
@@ -0,0 +1,71 @@
+from src.dualnum import *
+import pytest
+
+
+
+def test_sin_and_mul():
+    y=2*DualNum.sin(DualNum(0,1))+3
+    assert y.val == 3.0 and y.der == 2.0
+
+def test_sin2():
+    y=DualNum.sin(0)*2+3
+    assert y.val == 3.0 and y.der == 0.0
+
+def test_mul2():
+    y=DualNum(1,1)*DualNum(2,1)
+    assert y.val == 2 and y.der == 3 
+
+def test_rmul():
+    y=DualNum.sin(DualNum(0,1))*2+3
+    assert y.val == 3.0 and y.der == 2.0
+
+def test_cos():
+    y=2*DualNum.cos(DualNum(0,1))+3
+    assert y.val == 5.0 and y.der == 0.0
+
+def test_exp():
+    y=2*DualNum.exp(DualNum(0,1))+3
+    assert y.val == 5.0 and y.der == 2.0
+    y=2*DualNum.exp(0)+3
+    assert y.val == 5.0 and y.der == 0.0
+
+def test_ln():
+    y=2*DualNum.ln(DualNum(1,1))+3
+    assert y.val == 3.0 and y.der == 2.0
+    y=2*DualNum.ln(1)+3
+    assert y.val == 3.0 and y.der == 0.0
+
+def test_add_and_pow():
+    y=10*DualNum(1,1)**3+3*DualNum(1,1)**5
+    assert y.val == 13 and y.der == 45.0
+
+def test_pow2():
+    y=DualNum(1,1)**DualNum(2,1)
+    assert y.val == 1 and y.der == 2.0
+
+def test_radd():
+    y = 10 + DualNum(1,1)
+    assert y.val == 11 and y.der == 1
+
+def test_sub():
+    y =10-DualNum(1,1)
+    assert y.val == 9 and y.der == -1
+
+def test_rsub():
+    y =DualNum(1,1)-10
+    assert y.val == -9 and y.der == 1
+
+def test_neg():
+    y = DualNum.__neg__(DualNum(1,1))
+    assert y.val == -1 and y.der == -1
+
+def test_truediv():
+    y = DualNum(1,1)/10
+    assert y.val == 0.1 and y.der == 0.1
+
+def test_truediv2():
+    y = DualNum(1,1)/DualNum(4,2)
+    assert y.val == 0.25 and y.der == 0.125
+
+
+
