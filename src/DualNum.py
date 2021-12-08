42836fdf03d6dd3fdaab54064beacecd3170d0f6
Hannah Phan <hannahphan@dhcp-10-250-36-62.harvard.edu>
2021-12-08 15:01:17 -0500

    Fixed indentation error, added Forward class init

--- src/DualNum.py
+++ src/DualNum.py
@@ -5,76 +5,69 @@ class AutoDiff:
     def __init__(self, val, func, seed = None):
     
     '''
-	Initiates a function variable
-
-	Inputs
-	======
-	self: AutoDiff object
-	val: int or float; value of the user defined function(s) f evaluated at x = val
-	func: function(s) to be differentiated
-	seed: int, list, or array: the seed vector/derivates from parents
-
-	Notes
-	=====
-	For non-default seeds, the seed for forward mode is a dictionary of ints for each variable values, and the seed for reverse mode is a list of ints for each function.
-
+    Initiates a function variable
+    Inputs
+    ======
+    self: AutoDiff object
+    val: int or float; value of the user defined function(s) f evaluated at x = val
+    func: function(s) to be differentiated
+    seed: int, list, or array: the seed vector/derivates from parents
+    Notes
+    =====
+    For non-default seeds, the seed for forward mode is a dictionary of ints for each variable values, and the seed for reverse mode is a list of ints for each function.
     '''
-	forward_mode = True
-	if len(val) < len(func):
-	    print('The number of variables < The number of functions. Use forward mode.')
-	elif len(val) > len(func):
-	    print('The number of variables > The number of functions. Use reverse mode.')
-	else:
-	    print('The number of variables = The number of functions. Use forward mode.')
-	
-	if forward_mode:
-	    if (seed is not None) and (not isinstance(seed, dict)):
-		raise AttributeError('Forward mode needs a dictionary as a seed.')
-	    output = Forward(val, func, seed)
-	    else: 
-		if (seed is not None) and (not isinstance(seed, list)):
-		    raise AttributeError('Reverse mode needs a list as a seed with a seed for each function input.')
-		output = Reverse(val, func, seed)
+    forward_mode = True
+    if len(val) < len(func):
+        print('The number of variables < The number of functions. Use forward mode.')
+    elif len(val) > len(func):
+        print('The number of variables > The number of functions. Use reverse mode.')
+    else:
+        print('The number of variables = The number of functions. Use forward mode.')
+        
+    if forward_mode:
+        if (seed is not None) and (not isinstance(seed, dict)):
+            raise AttributeError('Forward mode needs a dictionary as a seed.')
+        output = Forward(val, func, seed)
+        else: 
+            if (seed is not None) and (not isinstance(seed, list)):
+                raise AttributeError('Reverse mode needs a list as a seed with a seed for each function input.')
+            output = Reverse(val, func, seed)
+            
+    self.output = output
+    self.val = val
+    self.der = der
+    
+    def __repr__(self):
+        return self.output.__repr__()
+    
+    def __str__(self):
+        return self.output.__str__()   
 
-	self.output = output
-	self.val = val
-	self.der = der
 
-   def __repr__(self):
-	return self.output.__repr__()
-
-   def __str__(self):
-	return self.output.__str__()   
-
- 
 class DualNum:
 
     '''
-	Creates a Dual Class that supports Auto Differentiation custom operations
-
-	Inputs
-	======
-	self: DualNum object
-	val: int or float; value of the user defined function(s) f evaluated at x = val
-	der: int or float; value of the initialized derivative/gradient/Jacobian of user defined function(s) f
-	seed: int, list, or array; the seed vector/derivative from parents	
+    Creates a Dual Class that supports Auto Differentiation custom operations
+    Inputs
+    ======
+    self: DualNum object
+    val: int or float; value of the user defined function(s) f evaluated at x = val
+    der: int or float; value of the initialized derivative/gradient/Jacobian of user defined function(s) f
+    seed: int, list, or array; the seed vector/derivative from parents
     '''
 
 
     def __init__(self, val, der, seed = np.array([1])):
+        if isinstance(val, (int, float)):
+            self.val = val
+        else:
+            raise TypeError('Did not enter valid int or float.')
+        if isinstance(seed, (int, float)):
+            seed = np.array([seed])
+        elif isinstance(seed, list):
+            seed = np.array(seed)
+        self.der = seed
         
-	if isinstance(val, (int, float)):
-	    self.val = val
-	else:
-	    raise TypeError('Did not enter valid int or float.')
-	if isinstance(seed, (int, float)):
-	    seed = np.array([seed])
-	elif isinstance(seed, list):
-	    seed = np.array(seed)
-	self.der = seed
-        
-
-	
     # Overload addition
     def __add__(self, other): 
         try:
@@ -117,11 +110,11 @@ class DualNum:
 
     # Overload rdiv
     def __rtruediv__(self, other):
-         try:
-             return DualNum(other.val / self.val, (other.der * self.val - other.val * self.der)/(self.val**2))
-         except:
-             other=DualNum(other,0)
-             return DualNum(other.val / self.val, (other.der * self.val - other.val * self.der)/(self.val**2))
+        try:
+            return DualNum(other.val / self.val, (other.der * self.val - other.val * self.der)/(self.val**2))
+        except:
+            other=DualNum(other,0)
+            return DualNum(other.val / self.val, (other.der * self.val - other.val * self.der)/(self.val**2))
 
     # Overload negation
     def __neg__(self):
@@ -140,23 +133,23 @@ class DualNum:
     # Overload rpow
     def __rpow__(self, exponent):
         try:
-          return DualNum(exponent.val ** self.val, exponent.val**self.val * (self.der * np.log(exponent.val) + (self.val / exponent.val) * exponent.der))
+            return DualNum(exponent.val ** self.val, exponent.val**self.val * (self.der * np.log(exponent.val) + (self.val / exponent.val) * exponent.der))
         except:
-          exponent=DualNum(exponent,0)
-          return DualNum(exponent.val ** self.val, exponent.val**self.val * (self.der * np.log(exponent.val) + (self.val / exponent.val) * exponent.der))
+            exponent=DualNum(exponent,0)
+            return DualNum(exponent.val ** self.val, exponent.val**self.val * (self.der * np.log(exponent.val) + (self.val / exponent.val) * exponent.der))
     
     # Overload equal
     def __eq__(self, other):
-	      try:
-	        output = (self.val == other.val) and np.array_equal(self.der, other.der)
-	      except AttributeError:
-	  # output is false because scalars are not equal to variables
-	      output = False
-	return output
+        try:
+            output = (self.val == other.val) and np.array_equal(self.der, other.der)
+        except AttributeError:
+            # output is false because scalars are not equal to variables
+            output = False
+        return output
 
     # Overload not equal
     def __ne__(self, other):
-	return not self.__eq__(other)
+        return not self.__eq__(other)
 
     # Overload sin
     @staticmethod  
@@ -197,112 +190,110 @@ class DualNum:
     # Overload tan
     @staticmethod
     def tan(other):
-	try:
-	    checkdomain = other.val % np.pi == (np.pi/2)
-	    if checkdomain:
-		raise ValueError(
-		    'Cannot take tangents of multiples of pi/2 + (pi * n), where n is a positive integer')
-	    new_other = np.tan(other.val)
-	    tan_deriv = 1 / np.power(np.cos(other.der), 2)
-	    new_der = other.der * tan_deriv
-	    
-            tan = DualNum(new_other, new_der)
-
-	    return tan
+        try:
+            checkdomain = other.val % np.pi == (np.pi/2)
+        if checkdomain:
+            raise ValueError('Cannot take tangents of multiples of pi/2 + (pi * n), where n is a positive integer')
+        new_other = np.tan(other.val)
+        tan_deriv = 1 / np.power(np.cos(other.der), 2)
+        new_der = other.der * tan_deriv
+        
+        tan = DualNum(new_other, new_der)
+        
+        return tan
 
         except AttributeError:
-	    return np.tan(other)
+        return np.tan(other)
 
     # Overload arcsin
     @staticmethod
     def arcsin(other):
-	try:
-	    if other.val > 1 or other.val <-1:
-	        raise ValueError('please use value between -1 and 1, inclusive')
-	    else:
-	        new_other = np.arcsin(other.val)
-     	        new_der = 1 / np.sqrt(1 - other.val**2)
-	
-	    arcsin = DualNum(new_other, new_der)
-	    return arcsin
-
-	except AttributeError:
-	    return np.arcsin(other)
+        try:
+            if other.val > 1 or other.val <-1:
+                raise ValueError('please use value between -1 and 1, inclusive')
+            else:
+                new_other = np.arcsin(other.val)
+                new_der = 1 / np.sqrt(1 - other.val**2)
+        
+            arcsin = DualNum(new_other, new_der)
+            return arcsin
     
-   # Overload arccos
-   @staticmethod
-   def arccos(other):
-	try:
-	    if other.val > 1 or other.val <-1:
-		raise ValueError('please use value between -1 and 1, inclusive')
-	    else:
-		new_other = np.arccos(other.val)
-		new_der = -1 / np.sqrt(1 - other.val**2)
-	    
-	    arccos = DualNum(new_other, new_der)
+        except AttributeError:
+            return np.arcsin(other)
+    
+    # Overload arccos
+    @staticmethod
+    def arccos(other):
+        try:
+            if other.val > 1 or other.val <-1:
+                raise ValueError('please use value between -1 and 1, inclusive')
+            else:
+                new_other = np.arccos(other.val)
+                new_der = -1 / np.sqrt(1 - other.val**2)
+        
+            arccos = DualNum(new_other, new_der)
             return arccos
+    
+        except AttributeError:
+            return np.arccos(other)
 
-	except AttributeError:
-	    return np.arccos(other)
-
-   # Overload arctangent
-   @staticmethod
-   def arctan(other):
-	try:
-	    new_other = np.arctan(other.val)
-	    arctan_deriv = 1 / (1 + np.power(other.val, 2))
-	    new_der = other.der * arctan_deriv
-	    
+    # Overload arctangent
+    @staticmethod
+    def arctan(other):
+        try:
+            new_other = np.arctan(other.val)
+            arctan_deriv = 1 / (1 + np.power(other.val, 2))
+            new_der = other.der * arctan_deriv
+            
             arctan = DualNum(new_other, new_der)
-	    return arctan
-	
-	except AttributeError:
-	    return np.arctan(other)
+            return arctan
+    
+        except AttributeError:
+            return np.arctan(other)
 
 
    # Overload sinh
    def sinh(other):
-	try:
-	    new_other = np.sinh(other.val)
-	    sinh_deriv = np.cosh(other.val)
-	    new_der = other.der * sinh_deriv
-	    
-	    sinh = DualNum(new_other, new_der)
-	    return sinh
-	except AttributeError:
-	    return np.sinh(other) 
-  
+        try:
+            new_other = np.sinh(other.val)
+            sinh_deriv = np.cosh(other.val)
+            new_der = other.der * sinh_deriv
+ 
+            sinh = DualNum(new_other, new_der)
+            return sinh
+        except AttributeError:
+            return np.sinh(other) 
+
    # Overload cosh
    def cosh(other):
-	try:
-	    new_other = np.cosh(other.val)
-	    cosh_deriv = np.sinh(other.val)
-	    new_der = other.der * cosh_deriv
-
-	    cosh = DualNum(new_other, new_der)
-	    return cosh
-	
-	except AtrributeError:
-	    return np.cosh(other)
+        try:
+            new_other = np.cosh(other.val)
+            cosh_deriv = np.sinh(other.val)
+            new_der = other.der * cosh_deriv
+            
+            cosh = DualNum(new_other, new_der)
+            return cosh
+        except AtrributeError:
+            return np.cosh(other)
 
    # Overload tanh
    def tanh(other):
-	try:
-	    new_other = np.tanh(other.val)
-	    tanh_deriv = 1 / np.power(np.cosh(other.val), 2)
-	    new_der = other.der * tanh_deriv
-
-	    tanh = DualNum(new_other, new_der)
-	    return tanh
-	
-	except AttributeError:
-	    return np.tanh(other)	 
+        try:
+            new_other = np.tanh(other.val)
+            tanh_deriv = 1 / np.power(np.cosh(other.val), 2)
+            new_der = other.der * tanh_deriv
+            
+            tanh = DualNum(new_other, new_der)
+            return tanh
+        
+        except AttributeError:
+            return np.tanh(other)
 
    # Overload sqrt
    def sqrt(other):
-	if other < 0:
-	    raise ValueError('Cannot take square roots of negative values')
-	return other ** (1/2)
+        if other < 0:
+            raise ValueError('Cannot take square roots of negative values')
+        return other ** (1/2)
 """
 #Some simple tests
 y=(DualNum.cos(2)**2.0 + 2**2.0)**0.5
@@ -314,6 +305,4 @@ print(y.val,y.der)
 y=(DualNum.cos(2)**DualNum(1,1))
 print(y.val,y.der)
 """
-
-
 
