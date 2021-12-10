45b3340b82f5b4a360725454fd6419ae7a041714
JieSun1990 <jie.sun1990@gmail.com>
2021-12-09 19:52:48 -0500

    2 tests

--- src/test_DualNum.py
+++ src/test_DualNum.py
@@ -1,71 +1,111 @@
 from DualNum import *
 import pytest
 
+# tested: cos, sin, sqrt, sinh, arcsin, cosh, sinh, tan
+# tested: mul, add, radd, sub, div, rdiv, pow, rpow, exp, 
+# tested: eq, ne, arcos, arctan, tanh, sqrt, 
+
+# forward mode
+def test_autodiff():
+    val = {'x': 0, 'y': 0.25}
+    func = ['cos(x) + y ** 2', '2 * sin(x) - sqrt(y)/3', '3 * sinh(y) - 4 * arcsin(y) + 5', 'cosh(x) / sinh(y)','tan(x)/3 - sqrt(y)']
+    output = AutoDiff(val, func)
+    print(output)
+
+def test_autodiff2():
+    val = {'x': 0, 'y': 0.25}
+    func = ['arccos(x) + y ** 2', '2 * arctan(x) - tanh(y)/3', '3 * sinh(y) - 4 * arcsin(y) + 5', 'cosh(x) / sinh(y)','tan(x)/3 - sqrt(y)']
+    output = AutoDiff(val, func)
+    repr(output)
+
+#not adding new
+def test_with_seed(): 
+    val = {'x': 10, 'y': 3}
+    seed = {'x': 1, 'y': 2}
+    func = ['cos(x) + y ** 2', 'tan(x)/3 - sqrt(y)']
+    output = AutoDiff(val, func, seed)
+
+def test_functionstring():
+    with pytest.raises(TypeError):
+        val = {'x': 1, 'y': 2}
+        func = ['cos(x) + y ** 2', 'tan(x)/3 - sqrt(y)',2]
+        output = Forward(val, func)
+
+def test_neg_sqrt():
+    with pytest.raises(ValueError):
+        DualNum.sqrt(-1)
+
+def test_neg_arccos():
+    with pytest.raises(ValueError):
+        DualNum.arccos(DualNum(-3,1))
+        DualNum.arcsin(DualNum(-3,1))
+
+def test_neg_arcsin():
+    with pytest.raises(ValueError):
+        DualNum.arcsin(DualNum(-3,1))
+
+def test_tan():
+    with pytest.raises(ValueError):
+        DualNum.tan(DualNum(np.pi/2 + (np.pi),1))
+
+def test_pow(): #didn't work
+    with pytest.raises(TypeError):
+        DualNum(-1,1)**DualNum(1,1)
+
+def test_init(): #didn't work
+    with pytest.raises(TypeError):
+        DualNum([1],1)
 
 
-def test_sin_and_mul():
-    y=2*DualNum.sin(DualNum(0,1))+3
-    assert y.val == 3.0 and y.der == 2.0
+def test_eq():
+    y1=2*DualNum.sin(DualNum(0,1))+3
+    y2=2*DualNum.sin(DualNum(0,1))+3
+    y3 = 10/DualNum(1,1)
+    print(y1 == y2, y1 != y3, y1 == 1)
 
-def test_sin2():
-    y=DualNum.sin(0)*2+3
-    assert y.val == 3.0 and y.der == 0.0
+def test_ne():
+    y1=2*DualNum.sin(DualNum(0,1))+3
+    y3 = 10/DualNum(1,1)
+    print(y1 != y3)
 
-def test_mul2():
-    y=DualNum(1,1)*DualNum(2,1)
-    assert y.val == 2 and y.der == 3 
-
+def test_sin_cos_arctan():
+    y1=2*DualNum.sin(0)+3
+    y2=2*DualNum.cos(0)+3
+    y3=2*DualNum.arctan(0)+3
+    y4=2*DualNum.sinh(0)+3
+    y4=2*DualNum.cosh(0)+3
+    
 def test_rmul():
     y=DualNum.sin(DualNum(0,1))*2+3
-    assert y.val == 3.0 and y.der == 2.0
-
-def test_cos():
-    y=2*DualNum.cos(DualNum(0,1))+3
-    assert y.val == 5.0 and y.der == 0.0
+    assert y.val == 3.0 
 
 def test_exp():
     y=2*DualNum.exp(DualNum(0,1))+3
-    assert y.val == 5.0 and y.der == 2.0
+    assert y.val == 5.0 
     y=2*DualNum.exp(0)+3
-    assert y.val == 5.0 and y.der == 0.0
+    assert y.val == 5.0 
 
 def test_ln():
     y=2*DualNum.ln(DualNum(1,1))+3
-    assert y.val == 3.0 and y.der == 2.0
+    assert y.val == 3.0
     y=2*DualNum.ln(1)+3
-    assert y.val == 3.0 and y.der == 0.0
-
-def test_add_and_pow():
-    y=10*DualNum(1,1)**3+3*DualNum(1,1)**5
-    assert y.val == 13 and y.der == 45.0
+    assert y.val == 3.0
 
 def test_pow2():
     y=3**DualNum(1,0)+5**DualNum(1,0)
-    assert y.val == 8 and y.der == 0.0
+    assert y.val == 8
 
 def test_radd():
     y = 10 + DualNum(1,1)
-    assert y.val == 11 and y.der == 1
-
-def test_sub():
-    y =10-DualNum(1,1)
-    assert y.val == 9 and y.der == -1
-
-def test_rsub():
-    y =DualNum(1,1)-10
-    assert y.val == -9 and y.der == 1
+    assert y.val == 11 
 
 def test_neg():
     y = DualNum.__neg__(DualNum(1,1))
-    assert y.val == -1 and y.der == -1
-
-def test_truediv():
-    y = DualNum(1,1)/10
-    assert y.val == 0.1 and y.der == 0.1
+    assert y.val == -1
 
 def test_rtruediv():
     y = 10/DualNum(1,1)
-    assert y.val == 10.0 and y.der == -10.0
+    assert y.val == 10.0
 
 
 
--- src/test_ReverseMode.py
+++ src/test_ReverseMode.py
@@ -1,10 +1,11 @@
 import pytest
 from ReverseMode import *
 
-
 # tested mult,exp,add,neg,y.der,rsub,
-# # adding radd didn't help
-# rneg? 
+# tested cos,sin,pow, div, reverse, sub,ln,rtruediv
+# tested cosh,sinh,tanh,arccos,arcsin,arctan,sqrt,logistic
+# adding radd/rmul/rpow/pow didn't help
+
 def test_exp():
     x1=Node(0)
     x2=Node(1)
@@ -12,15 +13,28 @@ def test_exp():
     y.reverse()
     assert y.val == 0.0 and y.getgrad(x2) == 0.0 and y.der['1'] == 1
 
-def test_pow(): #not really adding
+
+def test_radd_rmul_rsub_rpow():
+    x2=Node(2)
+    y1=2+x2
+    y2=2*x2
+    y3=2-x2
+    y4=2**x2
+    assert y1.val == 4 and y2.val == 4 and y3.val == 0 and y4.val == 4
+
+def test_pow(): 
     x1=Node(1)
     x2=Node(2)
     y=-x1**x2
     y.reverse()
     assert y.val == -1
 
-# tested cos,sin,pow, div, reverse, sub,ln,
-# rmul(didn't work),rtruediv 
+def test_logistic():
+    x1=Node(0)
+    y1=4*Node.logistic(x1)
+    y1.reverse()
+    assert y1.val == 2.0
+
 def test_vector():
     x1=Node(1)
     x2=Node(1)
@@ -29,6 +43,21 @@ def test_vector():
     f=NodeVec([y1,y2])
     f.reverse()
     assert f.getgrad(2,x2) == -5.0
+
+def test_vector2():
+    x1=Node(1)
+    x2=Node(1)
+    y1=4*x1*Node.arccos(x2)*Node.cosh(x1)-Node.arcsin(x1)*Node.arctan(x2)+Node.sinh(x1)*Node.tanh(x2)
+    y2=5*x1**2/x2+2+3**x1/1
+    f=NodeVec([y1,y2])
+    f.reverse()
+    assert f.getgrad(2,x2) == -5.0
+
+def test_sqrt():
+    x1=Node(1)
+    y1=4*Node.sqrt(x1)
+    y1.reverse()
+    assert y1.val == 4.0
 
 def test_variable_invalid():
     with pytest.raises(Exception):
@@ -63,4 +92,11 @@ def test_neg_val():
         x1=Node(-1)
         x2=Node(2)
         y=-x1**x2
+
+def test_sqrt2():
+    with pytest.raises(Exception):
+        x1=Node(-1)
+        y1=4*Node.sqrt(x1)
+
+
 
